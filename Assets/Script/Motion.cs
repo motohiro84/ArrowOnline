af2f7cff3eee59e101e2f75725bd7497c27c1ea8
motohiro84 <motogen.8040-1995@docomo.ne.jp>
2021-03-02 20:08:55 +0900

    弓モーションのスクリプト作成

---
+++ Assets/Script/Motion.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion : MonoBehaviour
+{
+  private Animator animator;
+  private string state;
+  AnimatorStateInfo animatorStateInfo;
+  void Start()
+  {
+    animator = this.gameObject.GetComponent<Animator>();
+  }
+
+
+  void Update()
+  {
+    animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+    if (GetInput())
+    {
+      Shot();
+    }
+
+  }
+
+  bool GetInput()
+  {
+    return Input.GetMouseButtonDown(0);
+  }
+
+  void Shot()
+  {
+    animator.SetTrigger("Shot");
+  }
+
+
+}
