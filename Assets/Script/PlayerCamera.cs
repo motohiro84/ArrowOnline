6f7e4c65845525d381aa8f82fa575c32c0e069fa
motohiro84 <motogen.8040-1995@docomo.ne.jp>
2021-03-08 16:01:55 +0900

    カメラの設定

--- Assets/Script/PlayerCamera.cs
+++ Assets/Script/PlayerCamera.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  public GameObject Player;
+  public GameObject Camera;
+  private Transform CameraTransform;
+  private float ii;
+
+  void Start()
+  {
+    CameraTransform = Camera.GetComponent<Transform>();
+    CameraTransform.Rotate(360f, 0, 0);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    TPS();
+  }
+
+  void TPS()
+  {
+    float X_Rotation = Input.GetAxis("Mouse X");
+    float Y_Rotation = Input.GetAxis("Mouse Y");
+    Player.transform.Rotate(0, X_Rotation, 0);
+
+    ii = (Camera.transform.localEulerAngles.x - Y_Rotation) * Mathf.Deg2Rad;
+    ii = Mathf.Sin(ii);
+
+    if (ii > -0.6f && ii < 0.2f)
     {
-        
+      Camera.transform.Rotate(-Y_Rotation, 0, 0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
 }
