01a6101edf49d1308e5d0a533b2509d253fea746
motohiro84 <motogen.8040-1995@docomo.ne.jp>
2021-03-11 13:19:53 +0900

    矢の発射

---
+++ Assets/Script/Shooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+  public GameObject bulletPrefab;
+  public float shotSpeed;
+  private float shotInterval;
+
+  void Update()
+  {
+    shotInterval += 1;
+    if (shotInterval > 18 && Input.GetMouseButtonDown(0))
+    {
+      ArrowShot();
+    }
+  }
+
+  void ArrowShot()
+  {
+
+    GameObject bullet = (GameObject)Instantiate(bulletPrefab, transform.position, Quaternion.Euler(transform.eulerAngles.x + 90, transform.eulerAngles.y, transform.eulerAngles.z - 15));
+    Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+    bulletRb.AddForce(transform.forward * shotSpeed);
+
+    Destroy(bullet, 3.0f);
+    shotInterval = 0;
+
+  }
+
+}
