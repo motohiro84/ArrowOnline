0db927f3d4d35f878a0882fea6d66efdb75200f0
motohiro84 <motogen.8040-1995@docomo.ne.jp>
2021-03-22 15:46:37 +0900

    レティクル変更

---
+++ Assets/Script/Reticle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reticle : MonoBehaviour
+{
+  public GameObject reticle;
+  public Sprite normal;
+  public Sprite lockOn;
+  public GameObject ArrowBox;
+
+  void Update()
+  {
+    Ray ray = new Ray(ArrowBox.transform.position, ArrowBox.transform.forward);
+
+    RaycastHit hit;
+
+    if (Physics.Raycast(ray, out hit))
+    {
+
+      if (hit.collider.tag == "Player")
+      {
+        reticle.GetComponent<Image>().sprite = lockOn;
+        reticle.GetComponent<Image>().color = Color.red;
+      }
+      else
+      {
+        reticle.GetComponent<Image>().sprite = normal;
+        reticle.GetComponent<Image>().color = Color.green;
+      }
+    }
+
+  }
+}
